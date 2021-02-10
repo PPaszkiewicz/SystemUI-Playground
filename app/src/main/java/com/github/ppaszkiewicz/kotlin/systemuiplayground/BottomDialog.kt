32b12cf49afe273f3aa43c73202f47e227bb94f0
PPaszkiewicz <pawel.paszkiewicz90@gmail.com>
2021-02-10 16:54:31 +0100

    Init

---
+++ app/src/main/java/com/github/ppaszkiewicz/kotlin/systemuiplayground/BottomDialog.kt
@@ -0,0 +1,134 @@
+package com.github.ppaszkiewicz.kotlin.systemuiplayground
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.os.Bundle
+import android.view.*
+import android.widget.FrameLayout
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.children
+import androidx.core.view.marginTop
+import androidx.core.view.updateLayoutParams
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+// dialog that has scrollable background behind nav bar
+class BottomDialog : BottomSheetDialogFragment() {
+    companion object {
+        fun isO() = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        if (isO()) {
+            val decor = requireDialog().window!!.decorView
+            decor.systemUiVisibility =
+                decor.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+        }
+        val container = requireDialog().findViewById<ViewGroup>(R.id.container)
+        container.fitsSystemWindows = false
+        (container.getChildAt(0) as ViewGroup).run {
+            // note: coordinator has systemUi FLAG_LAYOUT_FULLSCREEN set, that's why it's laid out on top
+            //of the nav bar
+            setOnApplyWindowInsetsListener { v, insets ->
+                // dispatch to all children
+                children.forEach { it.dispatchApplyWindowInsets(insets) }
+                insets
+            }
+            val clip = if (isO()) BottomDimView.PaddingClip.BEHIND_GRADIENT else
+                BottomDimView.PaddingClip.NONE
+            addView(BottomDimView(requireContext(), 0, clip))
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottomdialog, container, false)
+    }
+
+    @SuppressLint("ViewConstructor")
+    internal class BottomDimView(
+        context: Context,
+        val topOffset: Int,
+        val paddingClip: PaddingClip
+    ) : AppCompatTextView(context) {
+        enum class PaddingClip {
+            NONE, FULL_PADDING, BEHIND_GRADIENT
+        }
+
+        private val mBehavior = object : CoordinatorLayout.Behavior<View>() {
+            override fun layoutDependsOn(
+                parent: CoordinatorLayout,
+                child: View,
+                dependency: View
+            ): Boolean {
+                return dependency is FrameLayout
+            }
+
+            override fun onDependentViewChanged(
+                parent: CoordinatorLayout,
+                child: View,
+                dependency: View
+            ): Boolean {
+                if (child.height == 0) return false
+                child.elevation = dependency.elevation // clone dependency elevation
+                val depTop = dependency.top + topOffset + topPad(dependency)
+                val targetT = (depTop - child.top).coerceIn(0, child.height).toFloat()
+                return if (targetT != child.translationY) {
+                    child.translationY = targetT
+                    true
+                } else false
+            }
+
+            private fun topPad(v: View): Int {
+                val child = (v as ViewGroup).getChildAt(0) ?: return 0
+                return when (paddingClip) {
+                    PaddingClip.NONE -> 0
+                    PaddingClip.FULL_PADDING -> child.paddingTop
+                    PaddingClip.BEHIND_GRADIENT -> (child.paddingTop - (height / 4)).coerceAtLeast(0)
+                }
+            }
+        }
+
+        init {
+            layoutParams =
+                CoordinatorLayout.LayoutParams(CoordinatorLayout.LayoutParams.MATCH_PARENT, 0)
+                    .apply {
+                        behavior = mBehavior
+                        gravity = Gravity.BOTTOM
+                    }
+            gravity = Gravity.START or Gravity.TOP
+            fitsSystemWindows = true
+            setOnApplyWindowInsetsListener { v, insets ->
+                v.updateLayoutParams<ViewGroup.LayoutParams> {
+                    height = insets.systemWindowInsetBottom
+                }
+                text = "${insets.systemWindowInsetBottom}"
+                insets
+            }
+
+            // use black background on lower OS version because dark buttons can't be enforced
+            if (isO()) {
+                val c = bgColor(context)
+                val gradient = GradientDrawable(
+                    GradientDrawable.Orientation.TOP_BOTTOM,
+                    intArrayOf(Color.TRANSPARENT, c, c, c) // use four colors for 25% split
+                )
+                background = gradient
+                setTextColor(Color.BLACK)
+            } else {
+                setBackgroundColor(Color.BLACK)
+                setTextColor(Color.WHITE)
+            }
+        }
+
+        // color for white fade efect
+        fun bgColor(context: Context) = Color.WHITE
+    }
+}
---
+++ app/src/main/java/com/github/ppaszkiewicz/kotlin/systemuiplayground/SettingsActivity.kt
@@ -0,0 +1,212 @@
+package com.github.ppaszkiewicz.kotlin.systemuiplayground
+
+import android.os.Bundle
+import android.view.*
+import android.view.WindowManager.LayoutParams
+import android.widget.FrameLayout
+import android.widget.ListPopupWindow
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
+import androidx.core.content.edit
+import androidx.core.view.updatePadding
+import androidx.lifecycle.lifecycleScope
+import androidx.preference.*
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.settings, SettingsFragment())
+                    .commit()
+        }
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setListeners()
+    }
+
+    private fun setListeners(){
+        findViewById<Toolbar>(R.id.toolbar).setOnApplyWindowInsetsListener { v, insets ->
+            v.updatePadding(top = insets.systemWindowInsetTop)
+            insets
+        }
+        findViewById<FrameLayout>(R.id.settings).setOnApplyWindowInsetsListener { v, insets ->
+            v.updatePadding(top = insets.systemWindowInsetTop, bottom = insets.systemWindowInsetBottom)
+            insets
+        }
+        findViewById<FrameLayout>(R.id.topTextContainer).setOnApplyWindowInsetsListener { v, insets ->
+            v.updatePadding(top = insets.systemWindowInsetTop)
+            ((v as ViewGroup).getChildAt(0) as TextView).text = "${insets.systemWindowInsetTop}px"
+            insets
+        }
+        findViewById<FrameLayout>(R.id.bottomTextContainer).setOnApplyWindowInsetsListener { v, insets ->
+            v.updatePadding(bottom = insets.systemWindowInsetBottom)
+            ((v as ViewGroup).getChildAt(0) as TextView).text = "${insets.systemWindowInsetBottom}px"
+            insets
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                recreate()
+                true
+            }
+            R.id.dialog -> {
+                BottomDialog().show(supportFragmentManager, "BOTTOM")
+                true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        val window
+            get() = requireActivity().window
+
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            //general system ui
+            findPreference<SwitchPreferenceCompat>("stable_lay").systemUiFlag(View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+            findPreference<SwitchPreferenceCompat>("low_profile").systemUiFlag(View.SYSTEM_UI_FLAG_LOW_PROFILE)
+            findPreference<SwitchPreferenceCompat>("immersive").systemUiFlag(View.SYSTEM_UI_FLAG_IMMERSIVE)
+            findPreference<SwitchPreferenceCompat>("immersive_sticky").systemUiFlag(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            findPreference<SwitchPreferenceCompat>("lay_fullscreen").systemUiFlag(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+            // general window flags
+            findPreference<SwitchPreferenceCompat>("fullscreen").windowFlag(LayoutParams.FLAG_FULLSCREEN)
+            findPreference<SwitchPreferenceCompat>("not_fullscreen").windowFlag(LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+            findPreference<SwitchPreferenceCompat>("nolimit").windowFlag(LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+            findPreference<SwitchPreferenceCompat>("bounds").windowFlag(LayoutParams.FLAG_LAYOUT_IN_SCREEN)
+            findPreference<SwitchPreferenceCompat>("insetdecor").windowFlag(LayoutParams.FLAG_LAYOUT_INSET_DECOR)
+            findPreference<SwitchPreferenceCompat>("draw").windowFlag(LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+
+            // navigation
+            findPreference<SwitchPreferenceCompat>("n_hide").systemUiFlag(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+            findPreference<SwitchPreferenceCompat>("n_lay_hide").systemUiFlag(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+            findPreference<SwitchPreferenceCompat>("n_light").systemUiFlag(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR)
+            findPreference<SwitchPreferenceCompat>("n_trans").windowFlag(LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+            findPreference<SwitchPreferenceCompat>("n_contr").listen {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                    window.isNavigationBarContrastEnforced = it as Boolean
+                }
+            }
+            findPreference<ListPreference>("n_color").listen {
+                window.navigationBarColor = getColor(it as String)
+            }
+            findPreference<ListPreference>("n_divider_color").listen {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+                    window.navigationBarDividerColor = getColor(it as String)
+                }
+            }
+
+            // statusbar
+            findPreference<SwitchPreferenceCompat>("s_light").systemUiFlag(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+            findPreference<SwitchPreferenceCompat>("s_trans").windowFlag(LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            findPreference<SwitchPreferenceCompat>("s_contr").listen {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                    window.isStatusBarContrastEnforced = it as Boolean
+                }
+            }
+            findPreference<ListPreference>("s_color").listen {
+                window.statusBarColor = getColor(it as String)
+            }
+        }
+
+
+        // add uncancellable listener AND force invoke it
+        fun Preference?.listen(l: (value: Any) -> Unit) {
+            this!!.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, v ->
+                l(v)
+                true
+            }
+            this.sharedPreferences.all[key]?.let { l(it) }
+        }
+
+
+        fun SwitchPreferenceCompat?.systemUiFlag(flag: Int) {
+            this!!.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, v ->
+                if (v == true) {
+                    window.addSystemUiFlag(flag)
+                } else {
+                    window.clearSystemUiFlag(flag)
+                }
+                true
+            }
+            trackChanges{ window.decorView.systemUiVisibility and flag == flag }
+        }
+
+        fun SwitchPreferenceCompat?.windowFlag(flag: Int) {
+            this!!.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _, v ->
+                if (v == true) {
+                    window.addFlags(flag)
+                } else {
+                    window.clearFlags(flag)
+                }
+                true
+            }
+            trackChanges{ window.attributes.flags and flag == flag}
+        }
+
+        // track changes: start by invalidating self
+        private fun SwitchPreferenceCompat.trackChanges(chk: () -> Boolean){
+            sharedPreferences.getBoolean(key, false).let {
+                onPreferenceChangeListener.onPreferenceChange(this, it)
+            }
+            lifecycleScope.launch {
+                while(isActive){
+                    delay(500)
+                    val hasFlag = chk()
+                    if(isChecked != hasFlag){
+                        val l = onPreferenceChangeListener
+                        onPreferenceChangeListener = null
+                        isChecked = hasFlag
+                        onPreferenceChangeListener = l
+                    }
+                }
+            }
+        }
+        
+        fun getColor(color: String) = ContextCompat.getColor(requireContext(), when(color){
+                "Blue" -> R.color.blue
+                "Green" -> R.color.green
+                "Yellow50" -> R.color.yellow50
+                "Black" -> R.color.black
+                "White" -> R.color.white
+                "Transparent" -> R.color.transparent
+                else -> android.R.color.background_dark
+        })
+    }
+}
+
+
+fun Window.addSystemUiFlag(flag: Int) {
+    decorView.systemUiVisibility = decorView.systemUiVisibility or flag
+}
+
+fun Window.clearSystemUiFlag(flag: Int) {
+    decorView.systemUiVisibility = decorView.systemUiVisibility and flag.inv()
+}
+
+fun View.addSystemUiFlag(flag: Int) {
+    systemUiVisibility = systemUiVisibility or flag
+}
+
+fun View.clearSystemUiFlag(flag: Int) {
+    systemUiVisibility = systemUiVisibility and flag.inv()
+}
